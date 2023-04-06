d1869624c4b4777a5f7e7c62cf6565ba6b9c1211
renansouzzzz <renansouza.oliv@outlook.com>
2023-04-06 09:50:11 -0300

    initial commit

---
+++ src/main/java/br/com/kafkarestconsumer/KafkaRestConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.kafkarestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestConsumerApplication.class, args);
+	}
+
+}
